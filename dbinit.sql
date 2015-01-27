401306002c2b005ab3dacaba3c28cbe336910e56
duner <asduner@gmail.com>
2015-01-27 17:47:26 -0600

    Fix database configuratino stuff

--- dbinit.sql
+++ dbinit.sql
@@ -2,5 +2,6 @@ DROP DATABASE IF EXISTS frances;
 DROP USER IF EXISTS frances;
 CREATE ROLE frances PASSWORD 'frances' LOGIN;
 CREATE DATABASE frances OWNER frances;
+\c frances
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
